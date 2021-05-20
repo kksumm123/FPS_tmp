c0daf86c8858ca0f4fe01785ce7131d8449a483d
kksumm123 <kksumm123@gmail.com>
2021-05-20 20:57:58 +0900

    네비게이션 완료

---
+++ Assets/TargetEnemy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TargetEnemy : MonoBehaviour
+{
+    public Transform target;
+    public NavMeshAgent agent;
+    // 타겟을 매 프레임 쫒아가자.
+    IEnumerator Start()
+    {
+        while (true)
+        {
+            agent.destination = target.position;
+            //yield return new WaitForSeconds(1); //1초 쉬는거
+            yield return null; // 1프레임 쉬는거
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
