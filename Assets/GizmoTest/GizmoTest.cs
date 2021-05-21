8d60bd684e62954399480707bf23df3153b82d57
kksumm123 <kksumm123@gmail.com>
2021-05-21 17:14:47 +0900

    기즈모 스크립트 작성중

--- Assets/GizmoTest/GizmoTest.cs
+++ Assets/GizmoTest/GizmoTest.cs
@@ -4,9 +4,27 @@ using UnityEngine;
 
 public class GizmoTest : MonoBehaviour
 {
+    private float distance = 1;
+    public bool drawSphere = true;
+    public bool drawWireSphere = true;
+    public bool drawWireCube = true;
+    public bool drawRay = true;
+
+    private void OnDrawGizmosSelected()
+    {
+        if (drawSphere)
+            Gizmos.DrawSphere(transform.position, distance);
+        if (drawWireSphere)
+            Gizmos.DrawWireSphere(transform.position, distance);
+    }
     private void OnDrawGizmos()
     {
         Gizmos.color = Color.yellow;
-        Gizmos.DrawWireSphere(transform.position, 1);
+
+        if (drawWireCube)
+            //Vector3.one = new Vector3(1, 1, 1)
+            Gizmos.DrawWireCube(transform.position, Vector3.one * distance);
+        if (drawRay)
+            Gizmos.DrawRay(transform.position, Vector3.one * distance);
     }
 }
