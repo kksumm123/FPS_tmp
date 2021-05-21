843f78c3663f2392fac977eb5cdbce66683c8ce6
kksumm123 <kksumm123@gmail.com>
2021-05-21 17:06:43 +0900

    기즈모 스크립트 초안 완성

---
+++ Assets/GizmoTest/GizmoTest.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GizmoTest : MonoBehaviour
+{
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, 1);
+    }
+}
