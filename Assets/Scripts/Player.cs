fefba10acfaa6a115f43b7ab418ccf60112e08c9
kksumm123 <kksumm123@gmail.com>
2021-05-24 20:30:27 +0900

    엄청나다

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //플레이어 캐릭터 추가
+    //움직이기
+
+    //    기존 코드 재사용, 보는 방향으로 이동
+    //카메라 회전
+    //총쏘기
+
+    //    트레일 렌더러, 물리로 던지기
+    //수류탄 던지기
+    public float speed = 5f;
+    public float mouseSensitivity = 1f;
+
+    public Animator animator;
+    public Transform cameraTr;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        cameraTr = transform.Find("Main Camera");
+    }
+    private void Update()
+    {
+        Move();
+        CamaraRotate();
+    }
+
+    private void CamaraRotate()
+    {
+        // 카메라 로에티션을 바꾸자 - 마우스 이동량에 따라
+        float mouseMoveX = Input.GetAxisRaw("Mouse X") * mouseSensitivity;
+        float mouseMoveY = Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
+
+        // cameraTr
+        var worldUp = cameraTr.InverseTransformDirection(Vector3.up);
+        var rotation = cameraTr.rotation *
+                       Quaternion.AngleAxis(mouseMoveX, worldUp) *
+                       Quaternion.AngleAxis(mouseMoveY, Vector3.left);
+        transform.eulerAngles = new Vector3(0f, rotation.eulerAngles.y, 0f);
+        cameraTr.rotation = rotation;
+    }
+
+    private void Move()
+    {
+        // WASD, W위로, A왼쪽,S아래, D오른쪽
+        float moveX = 0;
+        float moveZ = 0;
+
+        // || -> or
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+            moveZ = 1;
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            moveZ = -1;
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            moveX = -1;
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            moveX = 1;
+
+        // 이 트렌스폼의 앞쪽으로 움직여야 한다
+        Vector3 move = transform.forward * moveZ
+                    + transform.right * moveX;
+        // 방향이니까 노멀라이즈해야한다
+        move.Normalize();
+        // 공간은 월드 절대좌표로
+        transform.Translate(move * speed * Time.deltaTime, Space.World);
+
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("shoot") == false)
+        {
+            if (moveX != 0 || moveZ != 0)
+                animator.Play("run");
+            else
+                animator.Play("idle");
+        }
+    }
+}
---
+++ Assets/Scripts/TargetEnemy.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public partial class TargetEnemy : MonoBehaviour
+{
+    public bool handle1;
+    public bool handle2;
+    public bool handle3;
+    public Vector3 direction;
+    public Transform player;
+    public NavMeshAgent agent;
+    private float moveSpeed;
+    public List<Transform> wayPoints;
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos()
+    {
+        if (EditorOption.Options[OptionType.디버그_라인그리기] == false)
+            return;
+
+        //Gizmos.DrawWireSphere(transform.position, viewingDistance);
+
+        // 시야각표시
+        // 호 표시.
+        //float
+        //Transform tr = GetComponent<Transform>(); // 같은 의미
+        Transform tr = transform;
+        float halfAngle = viewingAngle * 0.5f;
+        Handles.DrawWireArc(tr.position, tr.up, tr.forward.AngleToYDirection(-halfAngle)
+            , viewingAngle, viewingDistance);
+
+        // 오른쪽 왼쪽 선 표시.
+        Handles.DrawLine(tr.position, tr.position + tr.forward.AngleToYDirection(-halfAngle) * viewingDistance);
+        Handles.DrawLine(tr.position, tr.position + tr.forward.AngleToYDirection(halfAngle) * viewingDistance);
+    }
+#endif
+
+    public Animator animator;
+    public int wayPointIndex = 0;
+    // 타겟을 매프레임 쫒아가자.
+    IEnumerator Start()
+    {
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        moveSpeed = agent.speed;
+
+        yield return StartCoroutine(PetrolCo());
+    }
+
+    public float viewingDistance = 3;
+    public float viewingAngle = 90f; // 시야각
+    IEnumerator PetrolCo()
+    {
+        // 첫번째 웨이 포인트로 가자.
+        animator.Play("run");
+        while (true)
+        {
+            if (wayPointIndex >= wayPoints.Count)
+                wayPointIndex = 0;
+            agent.destination = wayPoints[wayPointIndex].position;
+            yield return null;
+            while (true)
+            {
+                if (agent.remainingDistance < 0.1f)
+                {
+                    Debug.Log("도착");
+                    // 2번째 웨이 포인트로 이동.
+                    break;
+                }
+
+                //플레이어 탐지.
+                // 플레이어와 나와의 위치를 구하자.
+                float distance = Vector3.Distance(transform.position, player.position);
+                if (distance < viewingDistance)
+                {
+                    // 시야각에 들어 왔다면 
+                    bool insideViewingAngle = false;
+                    // 시야각에 들어왔는지 확인하는 로직 넣자.
+
+                    Vector3 targetDir = player.position - transform.position;
+                    targetDir.Normalize();
+                    float angle = Vector3.Angle(targetDir, transform.forward);
+                    if (Mathf.Abs(angle) <= viewingAngle * 0.5f)
+                    {
+                        insideViewingAngle = true;
+                    }
+
+                    if (insideViewingAngle)
+                    {
+                        Debug.LogWarning("찾았다 -> 추적 상태로 전환");
+                    }
+                }
+
+                yield return null;
+            }
+            wayPointIndex++;
+        }
+    }
+}
