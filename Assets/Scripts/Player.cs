be720a392e953f196becd8a18091d6f278883c02
kksumm123 <kksumm123@gmail.com>
2021-05-24 20:36:52 +0900

    마우스잠금, 탭토글 추가

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -24,6 +24,7 @@ public class Player : MonoBehaviour
     {
         animator = GetComponent<Animator>();
         cameraTr = transform.Find("Main Camera");
+        Cursor.lockState = CursorLockMode.Locked;
     }
     private void Update()
     {
